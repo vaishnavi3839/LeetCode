c82a7d71ec9b7609c3ef38f831b2d983961670a0
vaishnavi3839 <vaishnavi9383@gmail.com>
2025-07-27 00:57:14 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (64.07%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.c
@@ -0,0 +1,23 @@
+bool isValid(char* s) {
+    int len = strlen(s);
+    char stack[len];
+    int top = -1;
+    for(int  i = 0 ; i < len; i++){
+        if ( s[i]== '('||s[i]=='{'||s[i]=='['){
+            stack[++top]=s[i];
+        }else {
+            if(top < 0) 
+                return false;
+ 
+            if( (s[i] == ')' && stack[top]!='(')||
+                (s[i]== '}' && stack[top]!='{')||
+                (s[i]== ']' && stack[top]!='['))
+                return false;
+            top--;
+        }
+
+    }
+    
+    return top == -1;
+
+}
